8d6907b25bc31693ac983b5b02f96da287e42a3a
Israel Marques Martins <email.israel.br@gmail.com>
2018-01-11 13:52:12 -0400

    Update calcmd.py

--- calcmd.py
+++ calcmd.py
@@ -1,11 +1,49 @@
 import calendar
 import datetime
+import subprocess
 
-week=["m","t","w","t","f","s","s"]
+week=["s","m","t","w","t","f","s"]
 
-def print_month():
-	for w in week:
-		print w,
+def create_table():
+	table=[]
+	for i in range(0,7):
+		table.append([])
+		for j in range(0,7):
+			table[i].append("0")
+	return table
+
+
+def print_table(table):
+
+	for i in range(0,7):
+		for j in range(0,7):
+			print table[i][j]+" ",
+		print '\n'
+
+
+def print_month(month,year):
+
+	table=create_table()
+
+
+	for i in range(0,7):
+		table[0][i]=week[i]
+
+	print(calendar.weekday(2016, 5, 15))
+
+	"""
+	i=0
+	j=1
+	d=1
+
+    while True:
+    	d=d+1
+    	table[i][j]=d
+    	"""
+
+
+
+	print_table(table)
 
 
 def main():
@@ -18,4 +56,4 @@ def main():
 	print m
 	print calendar.monthrange(y,2)[1]
 
-print_month()
+print_month("april",2015)
